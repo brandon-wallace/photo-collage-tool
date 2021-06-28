b3e253ac720b27d5840ceb1dcce057ab5c6490e4
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-06-27 23:41:57 -0400

    Improve CSS style

--- application/forms.py
+++ application/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import FileField
+from wtforms import FileField, SubmitField
 
 
 class UploadForm(FlaskForm):
     image = FileField('image')
+    submit = SubmitField('UPLOAD')
