e931b04c80672e058ad4a113b4f68abd8b0e9d17
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-07-08 23:17:53 -0400

    Add close flash message button

--- application/static/js/script.js
+++ application/static/js/script.js
@@ -1,13 +1,11 @@
 console.log('Javascript file enabled');
 
-//function handlerClick(type) {
-//    fetch('/tasks', {
-//        method: 'POST',
-//        headers: {
-//            'Content-Type': 'application/json'
-//        },
-//        body: JSON.stringify({ type: type }),
-//    })
-//    .then(response => response.json())
-//    .then(res => getStatus(res.data.task_id));
-//}
+
+// Close flash messages.
+const closeMessageButton = document.querySelector('.close');
+
+if (closeMessageButton) {
+    closeMessageButton.addEventListener('click', () => {
+        document.querySelector('.message').remove();
+    });
+}
