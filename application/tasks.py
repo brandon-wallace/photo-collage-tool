b8e51cdf502cc924e7b15c01078e65eaf329f83a
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-07-19 09:05:56 -0400

    Add functions
    Use smaller functions to remove duplicated code.
    Add rename_image, save_images_to, and create_thumbnail function.

--- application/tasks.py
+++ application/tasks.py
@@ -4,14 +4,37 @@ from application import celery
 from PIL import Image
 
 
+def rename_image(img, label):
+    '''Rename images'''
+
+    return f'{label}_{img}'
+
+
+def save_images_to(path='uploads/images'):
+    '''Set directory to save images to'''
+
+    location = Path(path)
+    return location
+
+
 # @celery.task
 def resize_image(img, size):
     '''Resize images'''
 
-    new_img = f'renamed_{img}'
-    location = Path('uploads/images')
+    new_img = rename_image('resized', img)
+    location = save_images_to()
     pic = Image.open(Path(location / img))
-    print(pic.filename)
     pic = pic.convert('RGBA')
     resized_picture = pic.resize((size, size))
     return resized_picture.save(Path(location / new_img), format='PNG')
+
+
+def create_thumbnail(img, size):
+    '''Create thumbnail images'''
+
+    new_img = rename_image('thumbnail', img)
+    location = save_images_to()
+    pic = Image.open(Path(location / img))
+    pic = pic.convert('RGBA')
+    thumbnail_image = pic.thumbnail()
+    return thumbnail_image.save(Path(location / new_img), format='PNG')
