f907db140bbd52c8332c528ed185b2fd07c36fff
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-08-28 05:14:40 -0400

    config: Celery integration
    Fully intergrate celery into a Flask application with blueprints
    and an application factory.

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from application import create_app
 
 app = create_app()
+app.app_context().push()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+from application import celery
