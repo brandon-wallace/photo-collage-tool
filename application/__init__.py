20257057a6a9277296d1be72bc2eae34313e291f
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-08-30 18:25:49 -0400

    Replace path
    Removed the hard coded path to the images. Path is set in the
    .env file.

--- application/__init__.py
+++ application/__init__.py
@@ -25,7 +25,7 @@ def create_app():
 
     app.config['DEBUG'] = True
     app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
-    app.config['UPLOADED_IMAGES_DEST'] = 'application/static/images/uploads'
+    app.config['UPLOADED_IMAGES_DEST'] = environ.get('UPLOADED_IMAGES_DEST')
     images = UploadSet('images', IMAGES)
     configure_uploads(app, images)
     celery.conf.update(app.config)
