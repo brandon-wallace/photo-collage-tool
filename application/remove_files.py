7a0492de0890abb1341dd0a6323d4aa033d711d6
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-09-10 23:35:23 -0400

    Log to /var/log
    Configured logging to write to /var/log/syslog.

--- application/remove_files.py
+++ application/remove_files.py
@@ -1,7 +1,15 @@
+import logging
+import logging.handlers
+
 from pathlib import Path
 from os.path import getmtime
 from datetime import datetime, timedelta
 
+logger = logging.getLogger('Logger')
+logger.setLevel(logging.INFO)
+
+handler = logging.handlers.SysLogHandler(address='/dev/log')
+logger.addHandler(handler)
 
 base_dir = Path('.').cwd()
 
@@ -22,7 +30,7 @@ total = sum([size + size for size in total_size])
 def convert_bytes(size):
     '''Convert bytes to megabytes'''
 
-    return f'Deleted total of: {(round(size / (1024 * 1024), 3))} MB'
+    return f'Total Deleted: {(round(size / (1024 * 1024), 3))} MB'
 
 
-print(convert_bytes(total))
+logger.info(convert_bytes(total))
