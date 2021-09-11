dd41321587e449f3fa91d959ec18a78a40808890
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-09-10 22:57:40 -0400

    Display total MB deleted
    Calculate the total number of megabytes deleted from the
    filesystem.

--- application/remove_files.py
+++ application/remove_files.py
@@ -5,10 +5,24 @@ from datetime import datetime, timedelta
 
 base_dir = Path('.').cwd()
 
-directory = Path(base_dir / 'application' / 'static' / 'images' / 'uploads')
+img_path = Path(base_dir / 'application' / 'static' / 'images' / 'uploads')
 
-for img in directory.iterdir():
+total_size = []
+
+for img in img_path.iterdir():
     modified_date_file = datetime.fromtimestamp(getmtime(img))
     if datetime.now() - modified_date_file > timedelta(hours=1):
+        total_size.append(img.stat().st_size)
         print(f'DELETING: {img.name}')
         # img.unlink()
+
+total = sum([size + size for size in total_size])
+
+
+def convert_bytes(size):
+    '''Convert bytes to megabytes'''
+
+    return f'Deleted total of: {(round(size / (1024 * 1024), 3))} MB'
+
+
+print(convert_bytes(total))
