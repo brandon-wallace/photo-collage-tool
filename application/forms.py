d072bc0dd2e2f877dcbc42c8551a190fdd0eb318
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-09-19 23:50:55 -0400

    Use flask-wtf for form
    The form was reconstructed using flask-wtf.

--- application/forms.py
+++ application/forms.py
@@ -1,7 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField
+from wtforms import (MultipleFileField, SubmitField,
+                     RadioField, IntegerField)
+from wtforms_components import ColorField
+from wtforms.validators import InputRequired, NumberRange
+from wtforms.widgets import html5
 
 
 class UploadForm(FlaskForm):
-    images = FileField('images')
+    images = MultipleFileField('images')
     submit = SubmitField('UPLOAD')
+
+
+class ImageSettingsForm(FlaskForm):
+    orientation = RadioField('Orientation', default='vertical',
+                             choices=[('vertical', 'Vertical Collage'),
+                                      ('horizontal', 'Horizontal Collage')])
+    background = ColorField('Background Color', validators=[InputRequired()])
+    border = IntegerField(validators=[NumberRange(min=0, max=100)])
+    border = IntegerField('Border', widget=html5.NumberInput(min=0, max=100,
+                                                             step=1))
+    submit = SubmitField('GENERATE COLLAGE')
