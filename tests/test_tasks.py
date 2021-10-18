f96ce57105a634561e74f895ea50e6254254d006
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-10-18 15:15:00 -0400

    Add Pytest fixture
    - Added a Pytest fixture to create a temporary directory and file.

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,5 +1,18 @@
+from os import environ
 from pathlib import Path
-from application.tasks import rename_image, save_image_to
+import pytest
+from PIL import Image
+from application.tasks import rename_image, save_path, resize_image
+
+
+@pytest.fixture(scope='session')
+def temp_file(tmpdir_factory):
+    '''Create a temporary image file'''
+
+    temp_image_obj = Image.new('RGB', size=(800, 600), color=(255, 0, 0))
+    temp_directory = tmpdir_factory.mktemp('temp_dir')
+    i = temp_image_obj.save(f'{temp_directory}{temp_image_obj}.jpg')
+    return i
 
 
 def test_rename_image():
@@ -7,6 +20,12 @@ def test_rename_image():
     assert rename_image('text', 'image') == 'text_image'
 
 
-def test_save_image_to():
+def test_save_path():
 
-    assert save_image_to('/') == Path('/')
+    assert Path(environ.get('UPLOADED_IMAGES_DEST')) == save_path()
+
+
+def test_resize_image(temp_file):
+
+    image = resize_image(temp_file, size=500, border=10, background='black')
+    assert image.mode == 'RGBA'
