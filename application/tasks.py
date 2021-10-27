fcbcb5b832f7d4cdeac5e9fb71cdd88c3eaea2c1
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-10-27 17:07:02 -0400

    Shorten line length
    - Import datetime as dt to shorten line length below 79
    characters.

--- application/tasks.py
+++ application/tasks.py
@@ -2,7 +2,7 @@
 
 from os import environ
 from pathlib import Path
-from datetime import datetime
+from datetime import datetime as dt
 from PIL import Image, ImageOps
 from application import celery
 
@@ -13,7 +13,6 @@ def save_path():
     return Path(environ.get('UPLOADED_IMAGES_DEST'))
 
 
-# @celery.task
 def resize_image(images, size, border, background=(0, 0, 0, 0)):
     '''Resize images to square shapes'''
 
@@ -25,35 +24,37 @@ def resize_image(images, size, border, background=(0, 0, 0, 0)):
         image_resized = image_png.resize((size, size))
         image_with_border = ImageOps.expand(image_resized, border=border,
                                             fill=(0, 0, 0, 0))
-        filename = f'resized_{datetime.utcnow().strftime("%Y%m%d-%H%M%S%f")}'
-        image_with_border.save(f'{upload_directory}/{filename}.png')
-        resized_images.append(f'{filename}.png')
-    return {'resized_images': resized_images}
+        filename = f'resized_{dt.utcnow().strftime("%Y%m%d-%H%M%S.%f")}.png'
+        image_with_border.save(f'{upload_directory}/{filename}')
+        resized_images.append(f'{filename}')
+    # return {'resized_images': resized_images}
+    return resized_images
 
 
-@celery.task
-def merge_images(image_list, orientation='horizontal'):
+@celery.task()
+def merge_images(image_list, filename, orientation='horizontal'):
     '''Merge images together'''
 
     upload_directory = save_path()
     image_objects = []
-    for filename in image_list:
-        img_obj = Image.open(f'{upload_directory}/{filename}')
+    for image_file in image_list:
+        img_obj = Image.open(f'{upload_directory}/{image_file}')
         image_objects.append(img_obj)
     if orientation == 'horizontal':
         total_width = sum(img.width for img in image_objects)
-        merged_image = Image.new('RGBA', (total_width, image_objects[0].height))
+        merged_image = Image.new('RGBA', (total_width,
+                                 image_objects[0].height))
         x_axis = 0
         for img in image_objects:
             merged_image.paste(img, (x_axis, 0))
             x_axis += img.width
     else:
         total_height = sum(img.height for img in image_objects)
-        merged_image = Image.new('RGBA', (image_objects[0].width, total_height))
+        merged_image = Image.new('RGBA', (image_objects[0].width,
+                                 total_height))
         y_axis = 0
         for img in image_objects:
             merged_image.paste(img, (0, y_axis))
             y_axis += img.height
-    filename = f'collage_{datetime.utcnow().strftime("%Y%m%d-%H%M%S.%f")}.png'
     merged_image.save(f'{upload_directory}/{filename}')
-    return {'filename': filename}
+    return
