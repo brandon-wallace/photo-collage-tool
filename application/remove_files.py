e486e25667f3bf1112209a40b3acec5a91a7d393
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-11-15 03:28:14 -0500

    Improve code
    - Add main function to call other function.
    - Add remove_old_files function
    - Add if __name__ == '__main__': check.

--- application/remove_files.py
+++ application/remove_files.py
@@ -21,16 +21,21 @@ base_dir = Path('.').cwd()
 
 images_path = Path(base_dir / 'application' / 'static' / 'images' / 'uploads')
 
-total_size = []
 
-for image in images_path.iterdir():
-    modified_date_file = datetime.fromtimestamp(getmtime(image))
-    if datetime.now() - modified_date_file > timedelta(hours=1):
-        total_size.append(image.stat().st_size)
-        print(f'DELETING: {image.name}')
-        # image.unlink()
+def remove_old_files(hours):
+    '''Remove old files to save space'''
 
-total = sum([size + size for size in total_size])
+    total_size = []
+
+    for image in images_path.iterdir():
+        modified_date_file = datetime.fromtimestamp(getmtime(image))
+        if datetime.now() - modified_date_file > timedelta(hours=hours):
+            total_size.append(image.stat().st_size)
+            print(f'DELETING: {image.name}')
+            image.unlink()
+
+    total = sum([size + size for size in total_size])
+    return total
 
 
 def convert_bytes(size):
@@ -45,4 +50,12 @@ def convert_bytes(size):
     return None
 
 
-logger.info(convert_bytes(total))
+def main():
+    '''Main function'''
+
+    total = remove_old_files(1)
+    logger.info(convert_bytes(total))
+
+
+if __name__ == '__main__':
+    main()
